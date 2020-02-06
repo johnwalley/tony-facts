b8e2ea1d396fe644a34e64cb1f7e132c9affa70c
John Walley <john@walley.org.uk>
2020-02-06 18:29:50 +0000

    Add submission link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Deck from './Deck';
+import './App.css';
 
 function App() {
-  return <Deck />;
+  return (
+    <>
+      <Deck />
+      <a
+        className="submit"
+        href="mailto:john@walley.org.uk?subject=I've%20got%20a%20new%20Tony%20Fact!"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Submit a Tony Fact™
+      </a>
+    </>
+  );
 }
 
 export default App;
